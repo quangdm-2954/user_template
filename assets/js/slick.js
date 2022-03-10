ff2b72a9e075df2c0ad3b4c0a2966c8349861d9c
quangdm-2954 <doan.minh.quang@sun-asterisk.com>
2022-03-10 14:50:37 +0700

    fix : style

--- assets/js/slick.js
+++ assets/js/slick.js
@@ -1,22 +1,68 @@
 $(function () {
   $(document).find('.your-playlist').slick({
     infinite: true,
-    slidesToShow: 6,
+    slidesToShow: 5,
     slidesToScroll: 2,
     autoplay: true,
     autoplaySpeed: 3000,
     pauseOnHover: true,
-    swipeToSlide: true
+    swipeToSlide: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 5,
+            slidesToScroll: 5,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+        }
+      }
+  ]
   });
 
   $(document).find('.v-pop').slick({
     infinite: true,
-    slidesToShow: 6,
+    slidesToShow: 5,
     slidesToScroll: 2,
     autoplay: true,
     autoplaySpeed: 4000,
     pauseOnHover: true,
     swipeToSlide: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 5,
+            slidesToScroll: 5,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+        }
+      }
+  ]
   });
 
   $(document).click(function (e) {
